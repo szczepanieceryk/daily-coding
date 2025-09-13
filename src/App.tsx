8ab5ec55eb8d39ff625b1da4fa56a9bd99470433
eryk <e.szczepaniec@lightpixel.io>
2025-09-13 16:08:39 +0200

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
+        <h1 className="text-xl font-bold text-center">🚀 Daily codeing project</h1>
+        <span className="block text-center">
+          Here I add small parts of code, everyday, from 10 to 15min{' '}
+        </span>
       </div>
-      <Outlet />
     </div>
   );
 };
