2b8e1358679938f1e9c2113b4d04182e900fff40
eryk <szczepanieceryk@gmail.com>
2025-09-23 20:53:21 +0200

    fix(ui): change buttons themes  on clink

--- src/layouts/ThemeSwitcher.tsx
+++ src/layouts/ThemeSwitcher.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 
+type Theme = 'light' | 'dark';
+
 const ThemeSwitcher: React.FC = () => {
-  const [currentTheme, setCurrenTheme] = useState<string>('light');
+  const [currentTheme, setCurrentTheme] = useState<Theme>('light');
 
-  //   const buttonThemes = { light: '', dark: '' };
+  const buttonThemes: Record<Theme, string> = {
+    light: 'bg-blue-500 text-white',
+    dark: 'bg-gray-800 text-white',
+  };
 
-  //   const buttonStyles = buttonThemes[currentTheme];
+  const buttonStyles = buttonThemes[currentTheme];
   const baseClasses = 'rounded-md py-3 px-4 bg-gray-200';
-  const buttonClasses = `${baseClasses} 'buttonStyles'`;
+  const buttonClasses = `${baseClasses} ${buttonStyles}`;
 
   const handleClick = () => {
-    setCurrenTheme('');
+    setCurrentTheme('dark');
   };
   return (
     <div className="my-6 p-4 rounded-md border-2 border-gray-100">
