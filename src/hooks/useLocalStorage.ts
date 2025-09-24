c7c5bdaabc659693bb304f8c5b3111e0d71937c6
eryk <szczepanieceryk@gmail.com>
2025-09-24 09:51:36 +0200

    feat: create custom hook useLocalStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key: string, defaultValue: string) => {
+  const [value, setValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : defaultValue;
+    } catch (error) {
+      console.error(`Error reading localStorage key "${key}":`, error);
+      return defaultValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Error setting local storage "${key}:"`, error);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
