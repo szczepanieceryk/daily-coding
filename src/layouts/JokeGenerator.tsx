c27e47fabb3c50945747fa54dedcea323e231488
eryk <szczepanieceryk@gmail.com>
2025-10-02 11:11:21 +0200

    feat(API): make request to API & disaply the joke

--- src/layouts/JokeGenerator.tsx
+++ src/layouts/JokeGenerator.tsx
@@ -1,11 +1,34 @@
 import React, { useState } from 'react';
 import Button from '../components/Button';
 
+interface FormResponse {
+  id: string;
+  joke: string;
+  status: number;
+}
+
 const JokeGenerator = () => {
   const [generatedJoke, setGeneratedJoke] = useState<string>('');
-  const handleSubmit = (e: React.FormEvent): void => {
-    e.preventDefault;
-    setGeneratedJoke('This is generated joke');
+  const handleSubmit = async (e: React.FormEvent): Promise<void> => {
+    e.preventDefault();
+
+    try {
+      const response: Response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data: FormResponse = await response.json();
+      setGeneratedJoke(data.joke);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      console.error(`Error during joke generation: ${error}`);
+    }
   };
   return (
     <div className="p-4 rounded-lg border-2 border-gray-200 text-center">
