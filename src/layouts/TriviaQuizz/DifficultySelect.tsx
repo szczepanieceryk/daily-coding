6a4bfd2bd3e751e1b7485845baba22f3239fcf3a
eryk <szczepanieceryk@gmail.com>
2025-10-16 13:54:51 +0200

    fix(ui): display difficulty levels dynamically

--- src/layouts/TriviaQuizz/DifficultySelect.tsx
+++ src/layouts/TriviaQuizz/DifficultySelect.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 interface DifficultySelectProps {
   handleChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
+
+const difficultyLevels = ['easy', 'medium', 'hard'];
+
 const DifficultySelect: React.FC<DifficultySelectProps> = ({ handleChange }) => {
   return (
     <label htmlFor="question-difficulty-id">
@@ -13,9 +16,11 @@ const DifficultySelect: React.FC<DifficultySelectProps> = ({ handleChange }) => 
         className="block p-2 rounded-md my-2 mx-auto max-w-[350px] cursor-pointer text-center bg-gray-700 text-white"
         onChange={handleChange}
       >
-        <option value="easy">Easy</option>
-        <option value="medium">Medium</option>
-        <option value="hard">Hard</option>
+        {difficultyLevels?.map?.((lvl) => (
+          <option key={lvl} value={lvl}>
+            {lvl.charAt(0).toUpperCase() + lvl.slice(1)}
+          </option>
+        ))}
       </select>
     </label>
   );
