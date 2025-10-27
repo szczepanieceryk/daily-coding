46412cdda94d78391d555427c42c7b67f03e9fe7
eryk <szczepanieceryk@gmail.com>
2025-10-27 16:24:07 +0100

    feat(ui): dont display button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { createContext } from 'react';
-import Button from './components/Button';
 import NewsletterForm from './layouts/NewsletterForm';
 import AutoCompleateSearch from './layouts/AutoCompleteSearch';
 import useLocalStorage from './hooks/useLocalStorage';
@@ -31,28 +30,6 @@ const App = () => {
         <h1 className="text-xl font-bold text-center">🚀 Daily codeing project</h1>
         <span className="block text-center">
           Here I add small parts of code, everyday, from 10 to 15min{' '}
-        </span>
-
-        <span className="block my-[2em]">
-          <strong>Components:</strong>
-        </span>
-
-        <span className="block text-center">
-          <strong>Buttons</strong>
-        </span>
-        <div className="my-[1em] flex flex-wrap justify-center md:justify-between max-w-[250px] md:max-w-[550px] mx-auto">
-          {/* Primary button */}
-          <Button content="Primary button" style="primary" type="button" />
-
-          {/* Secondary button */}
-          <Button content="Secondary button" style="secondary" type="button" />
-
-          {/* Disabled button */}
-          <Button content="Disabled button" style="primary" type="button" disabled={true} />
-        </div>
-
-        <span className="block mt-[4em]">
-          <strong>Code snipets with functionality:</strong>
         </span>
 
         <span className="block text-center">
