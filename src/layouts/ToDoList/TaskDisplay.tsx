d195c03118e2dc3ff4b6418b51a66725f631becf
eryk <szczepanieceryk@gmail.com>
2025-11-10 12:28:05 +0100

    refactor: change span element for input label

--- src/layouts/ToDoList/TaskDisplay.tsx
+++ src/layouts/ToDoList/TaskDisplay.tsx
@@ -18,15 +18,17 @@ const TaskDisplay: React.FC<TaskProps> = ({ id, task, onDelete, selectTask }) =>
   return (
     <div className="p-3 my-3 mx-auto flex justify-between items-center bg-gray-700 rounded-lg max-w-[300px]">
       <div className="text-left">
-        <input
-          onChange={handleCheckboxChange}
-          type="checkbox"
-          name=""
-          id={id}
-          className="mr-2"
-          value={task}
-        />
-        <span className="inline">{task}</span>
+        <label className="inline cursor-pointer" htmlFor={id}>
+          <input
+            onChange={handleCheckboxChange}
+            type="checkbox"
+            name=""
+            id={id}
+            className="mr-2"
+            value={task}
+          />
+          {task}
+        </label>
       </div>
       <div
         onClick={handleDeleteTask}
